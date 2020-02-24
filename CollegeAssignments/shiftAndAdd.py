b3f0adc539a0bd742828c8dbd9ea1f6b84f0332d
NamanLad <namanlad@gmail.com>
2020-02-24 18:49:20 +0530

    shift and add logic updated

--- CollegeAssignments/shiftAndAdd.py
+++ CollegeAssignments/shiftAndAdd.py
@@ -1,5 +1,48 @@
 # aim is to multiply q and m
 
+
+"""
+using full adder logic
+for adding a, b, c
+sum = a XOR b XOR c
+carry = ab + c*(a XOR b)
+"""
+
+
+def add(a, m):
+    c = 0
+    for i in range(length-1, -1, -1):
+        num1 = int(a[i])
+        num2 = int(m[i])
+
+        sum = num1 ^ num2 ^ c
+        carry = num1 & num2 | (c & (num1 ^ num2))
+        a[i] = str(sum)
+        c = carry
+    return str(c)
+
+
+def display(c, a, q):
+
+    # print('ans is:')
+    print()
+    print(c, end=" ")
+    for i in a:
+        print(i, end="")
+    print(end=" ")
+    for i in q:
+        print(i, end="")
+
+
+def right_shift(c, a, q):
+
+    q = [a[-1]] + q[:-1]
+    a = [c] + a[:-1]
+    c = '0'
+
+    return c, a, q
+
+
 q = list(input('Enter q'))
 m = list(input('Enter m'))
 
@@ -8,35 +51,19 @@ a = list()
 for _ in range(length):
     a.append('0')
 
-carry = 0
 c = '0'
 
 for _ in range(length):
+    display(c, a, q)
     bit = q[-1]
 
     if bit == '1':
-        for i in range(length-1, -1, -1):
-            num1 = int(a[i])
-            num2 = int(m[i])
-            addition = num1 + num2 + carry
+        c = add(a, m)
 
-            if addition == 0 or addition == 1:
-                carry = 0
-                c = '0'
-                char = str(addition)
-            elif addition == 2 or addition == 3:
-                carry = 1
-                c = '1'
-                char = str(addition-2)
-            a[i] = char
+    c, a, q, = right_shift(c, a, q)
 
-    q = [a[3]] + q[:3]
-    a = [c] + a[:3]
+display(c, a, q)
 
-print('ans is:')
-print(c, end="")
-for i in a:
-    print(i, end="")
-for i in q:
-    print(i, end="")
+
+
 
